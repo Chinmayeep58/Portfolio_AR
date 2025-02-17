7cd9f33e36656c5f82face9518bc4572273ba511
Chinmayeep58 <chinmayee05rao@gmail.com>
2025-02-17 19:37:57 +0530

    first commit

---
+++ Assets/Scripts/LinkManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void openLinkedIn(){
+        Application.OpenURL("https://www.linkedin.com/in/chinmayee-p-6a87a6229/");
+    }
+
+    public void openGithub(){
+        Application.OpenURL("https://github.com/Chinmayeep58");
+    }
+
+    public void openX(){
+        Application.OpenURL("https://x.com/Chinmayee_58?t=SPD0SP-XTWOTgsL7C9CQgA&s=08");
+    }
+
+    public void openDiscord(){
+        Application.OpenURL("https://www.linkedin.com/in/chinmayee-p-6a87a6229/");
+    }
+}
